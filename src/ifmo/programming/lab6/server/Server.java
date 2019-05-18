c5f37174cd0ee6444f04b8a4bef7e31f019b0e44
Nadya <mmmlpmsw@gmail.com>
2019-05-19 01:08:17 +0300

    fix loading from env

--- src/ifmo/programming/lab6/server/Server.java
+++ src/ifmo/programming/lab6/server/Server.java
@@ -1,21 +1,28 @@
 package ifmo.programming.lab6.server;
 
-
-
 import ifmo.programming.lab6.transmitter.Receiver;
 import ifmo.programming.lab6.transmitter.ReceiverListener;
 import ifmo.programming.lab6.transmitter.Sender;
 import ifmo.programming.lab6.transmitter.SenderAdapter;
 import ifmo.programming.lab6.Message;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.nio.file.AccessDeniedException;
+import java.util.Map;
+
+import static ifmo.programming.lab6.server.BuildingChecker.getCollectionFromJSON;
 
 public class Server {
     private static final int RECEIVING_PORT = 2222;
     static Building building = new Building();
+    private static final String FILENAME_ENV = "LOADFILE";
+    private static final String FILE_FOR_AUTOSAVE = "autosave.json";
 
     public static void main(String[] args) {
+
         try {
             Receiver receiver = new Receiver(RECEIVING_PORT, false);
             receiver.setListener(generateListener());
@@ -24,6 +31,27 @@ public class Server {
             System.out.println("Сервер слушает порт " + RECEIVING_PORT + "...");
         } catch (IOException e) {
             System.out.println("Не получилось запустить сервер: " + e.toString());
+        }
+
+        Map<String, String> env = System.getenv();
+        String filename = env.get(FILENAME_ENV);
+
+        if (filename != null && !filename.isEmpty()) {
+            try {
+                String content = getContentFromFile(filename);
+                getCollectionFromJSON(building, content);
+                System.out.println("Состояние загружено из файла");
+                System.out.println("Загружено " + building.getSize() + " комнат");
+
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл не найден");
+            } catch (AccessDeniedException e) {
+                System.out.println("Отсутствует соответствующий доступ к файлу");
+            } catch (IOException e) {
+                System.out.println("Ошибка чтения/записи");
+            } catch (Exception e) {
+                System.out.println("Не получилось загрузить состояние: " + e.getMessage());
+            }
         }
     }
 
@@ -64,5 +92,21 @@ public class Server {
                 System.out.println("Не получилось принять запрос: " + e.toString());
             }
         };
+    }
+
+
+    protected static String getContentFromFile(String filename) throws IOException {
+        try (FileReader reader = new FileReader(filename)) {
+            String filecontent = new String("");
+            int running;
+            do {
+                running = reader.read();
+                if (running != -1) {
+                    filecontent = filecontent.concat(String.valueOf((char)running));
+
+                }
+            } while (running != -1);
+            return filecontent;
+        }
     }
 }
